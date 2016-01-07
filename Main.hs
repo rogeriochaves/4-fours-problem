79defe78c59750159726ba3b8c2fc03d1a5059f5
Rogério Chaves <rogeriocfj@gmail.com>
2016-01-07 02:42:02 -0200

    Accept operations with any amount of arity

--- Main.hs
+++ Main.hs
@@ -4,42 +4,58 @@ import Data.Maybe
 
 type Value = Double
 
-data Operation = Sum | Minus | Division | Multiplication | Pow | Root | Concat deriving (Eq, Ord)
+data Operation a = NoOp
+                 | Sum            (Operation a) (Operation a)
+                 | Minus          (Operation a) (Operation a)
+                 | Division       (Operation a) (Operation a)
+                 | Multiplication (Operation a) (Operation a)
+                 | Pow            (Operation a) (Operation a)
+                 | Root           (Operation a) (Operation a)
+                 | Concat         (Operation a) (Operation a)
+                 | Square         (Operation a)
+                 | SquareRoot     (Operation a)
 
-data Expressions a = Four | Expression (Expressions a) a (Expressions a)
+singleops = [Square, SquareRoot]
+doubleops = [Sum, Minus, Division, Multiplication, Pow, Root, Concat]
 
-instance Show Operation where
-  show Sum = " + "
-  show Minus = " - "
-  show Division = " / "
-  show Multiplication = " * "
-  show Pow = " ^ "
-  show Root = " √ "
-  show Concat = " ++ "
+type AmountOfFours = Int
+type Result = (AmountOfFours, Value)
 
-instance Show (Expressions Operation) where
-  show Four = "4"
-  show (Expression x op y) = "(" ++ (show x) ++ (show op) ++ (show y) ++ ")"
+eval :: Operation Value -> Result
+eval NoOp                 = (1, 4)
+eval (Sum x y)            = (foursCount [x, y], result x + result y)
+eval (Minus x y)          = (foursCount [x, y], result x - result y)
+eval (Division x y)       = (foursCount [x, y], result x / result y)
+eval (Multiplication x y) = (foursCount [x, y], result x * result y)
+eval (Pow x y)            = (foursCount [x, y], result x ** result y)
+eval (Root x y)           = (foursCount [x, y], result x ** (1 / (result y)))
+eval (Concat x y)         = (foursCount [x, y], read $ (show (floor (result x))) ++ (show (abs (floor (result y)))))
+eval (Square x)           = (foursCount [x]   , (result x) ** 2)
+eval (SquareRoot x)       = (foursCount [x]   , sqrt (result x))
 
-evalOperation :: Value -> Operation -> Value -> Value
-evalOperation x Sum y            = x + y
-evalOperation x Minus y          = x - y
-evalOperation x Division y       = x / y
-evalOperation x Multiplication y = x * y
-evalOperation x Pow y            = x ** y
-evalOperation x Root y           = x ** (1/y)
-evalOperation x Concat y         = read $ (show (floor x)) ++ (show (abs (floor y)))
+instance Show (Operation Value) where
+  show NoOp                 = "4"
+  show (Sum x y)            = "(" ++ (show x) ++ " + " ++ (show y) ++ ")"
+  show (Minus x y)          = "(" ++ (show x) ++ " - " ++ (show y) ++ ")"
+  show (Division x y)       = "(" ++ (show x) ++ " / " ++ (show y) ++ ")"
+  show (Multiplication x y) = "(" ++ (show x) ++ " * " ++ (show y) ++ ")"
+  show (Pow x y)            = "(" ++ (show x) ++ " ^ " ++ (show y) ++ ")"
+  show (Root x y)           = "(" ++ (show x) ++ "√ " ++ (show y) ++ ")"
+  show (Concat x y)         = "(" ++ (show x) ++ " ++ " ++ (show y) ++ ")"
+  show (Square x)           = (show x) ++ "²"
+  show (SquareRoot x)       = "√" ++ (show x)
 
-operations :: [Operation]
-operations = [Sum, Minus, Division, Multiplication, Pow, Root, Concat]
+result :: Operation Value -> Value
+result = snd . eval
 
-possibleExpressions :: Operation -> Operation -> Operation -> [Expressions Operation]
-possibleExpressions a b c = [(Expression Four a (Expression Four b (Expression Four c Four))),
-                             (Expression (Expression Four b Four) a (Expression Four c Four))]
+foursCount :: [Operation Value] -> Int
+foursCount ops = sum $ fmap (fst . eval) ops
 
-evalExpression :: Expressions Operation -> Value
-evalExpression Four = 4
-evalExpression (Expression x op y) = evalOperation (evalExpression x) op (evalExpression y)
+possibleCombinations1 :: [Operation Value]
+possibleCombinations1 = [NoOp] ++ [ x NoOp | x <- singleops ]
 
-findOperationsWithFourResultingIn :: Value -> Maybe (Expressions Operation)
-findOperationsWithFourResultingIn x = listToMaybe [ e | a <- operations, b <- operations, c <- operations, e <- (possibleExpressions a b c), x == evalExpression e ]
+possibleCombinations2 :: [Operation Value] -> [Operation Value]
+possibleCombinations2 []     = [ op x y | op <- doubleops, x <- possibleCombinations1, y <- possibleCombinations1 ]
+possibleCombinations2 (x:xs) = [ op x y | op <- doubleops, x <- (possibleCombinations1 ++ possibleCombinations2 xs), y <- (possibleCombinations1 ++ possibleCombinations2 xs) ]
+
+findOperationWithFourResultingIn x = listToMaybe [ op | op <- possibleCombinations2 [NoOp, NoOp], result op == x, foursCount [op] == 4 ]
