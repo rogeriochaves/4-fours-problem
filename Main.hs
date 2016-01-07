f4e8501df53ca5ed8cae388bb90403b33203c748
Rogério Chaves <rogeriocfj@gmail.com>
2016-01-07 00:14:04 -0200

    Add pow, root and concat

--- Main.hs
+++ Main.hs
@@ -1,25 +1,37 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 import Data.Maybe
 
 type Value = Double
 
-data Operation = Sum | Minus | Division | Multiplication deriving (Eq, Ord)
+data Operation = Sum | Minus | Division | Multiplication | Pow | Root | Concat deriving (Eq, Ord)
 
-data Expressions a = Four | Expression (Expressions a) a (Expressions a) deriving (Show)
+data Expressions a = Four | Expression (Expressions a) a (Expressions a)
 
 instance Show Operation where
-  show Sum = "+"
-  show Minus = "-"
-  show Division = "/"
-  show Multiplication = "*"
+  show Sum = " + "
+  show Minus = " - "
+  show Division = " / "
+  show Multiplication = " * "
+  show Pow = " ^ "
+  show Root = " √ "
+  show Concat = " ++ "
+
+instance Show (Expressions Operation) where
+  show Four = "4"
+  show (Expression x op y) = "(" ++ (show x) ++ (show op) ++ (show y) ++ ")"
 
 evalOperation :: Value -> Operation -> Value -> Value
 evalOperation x Sum y            = x + y
 evalOperation x Minus y          = x - y
 evalOperation x Division y       = x / y
 evalOperation x Multiplication y = x * y
+evalOperation x Pow y            = x ** y
+evalOperation x Root y           = x ** (1/y)
+evalOperation x Concat y         = read $ (show (floor x)) ++ (show (abs (floor y)))
 
 operations :: [Operation]
-operations = [Sum, Minus, Division, Multiplication]
+operations = [Sum, Minus, Division, Multiplication, Pow, Root, Concat]
 
 possibleExpressions :: Operation -> Operation -> Operation -> [Expressions Operation]
 possibleExpressions a b c = [(Expression Four a (Expression Four b (Expression Four c Four))),
@@ -29,9 +41,5 @@ evalExpression :: Expressions Operation -> Value
 evalExpression Four = 4
 evalExpression (Expression x op y) = evalOperation (evalExpression x) op (evalExpression y)
 
-head' :: [a] -> Maybe a
-head' [] = Nothing
-head' (x:xs) = Just x
-
 findOperationsWithFourResultingIn :: Value -> Maybe (Expressions Operation)
-findOperationsWithFourResultingIn x = head' [ e | a <- operations, b <- operations, c <- operations, e <- (possibleExpressions a b c), x == evalExpression e ]
+findOperationsWithFourResultingIn x = listToMaybe [ e | a <- operations, b <- operations, c <- operations, e <- (possibleExpressions a b c), x == evalExpression e ]
