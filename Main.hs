3786c271e76f4020346e0e91d2a9fc62ab58c7bd
Rogério Chaves <rogeriocfj@gmail.com>
2016-01-07 11:41:44 -0200

    Remove square and concat, add factorial and termial

--- Main.hs
+++ Main.hs
@@ -1,61 +1,64 @@
 {-# LANGUAGE FlexibleInstances #-}
 
-import Data.Maybe
+import Data.Maybe (listToMaybe)
+import Control.Applicative ((<$>))
 
 type Value = Double
 
-data Operation a = NoOp
+data Operation a = Four
                  | Sum            (Operation a) (Operation a)
                  | Minus          (Operation a) (Operation a)
                  | Division       (Operation a) (Operation a)
                  | Multiplication (Operation a) (Operation a)
                  | Pow            (Operation a) (Operation a)
                  | Root           (Operation a) (Operation a)
-                 | Concat         (Operation a) (Operation a)
-                 | Square         (Operation a)
+                 | Factorial      (Operation a)
                  | SquareRoot     (Operation a)
+                 | Termial        (Operation a)
 
-singleops = [Square, SquareRoot]
-doubleops = [Sum, Minus, Division, Multiplication, Pow, Root, Concat]
+noops = [Four]
+singleops = [Factorial, SquareRoot, Termial]
+doubleops = [Sum, Minus, Division, Multiplication, Pow, Root]
 
 type AmountOfFours = Int
 type Result = (AmountOfFours, Value)
 
 eval :: Operation Value -> Result
-eval NoOp                 = (1, 4)
+eval Four                 = (1, 4)
 eval (Sum x y)            = (foursCount [x, y], result x + result y)
 eval (Minus x y)          = (foursCount [x, y], result x - result y)
 eval (Division x y)       = (foursCount [x, y], result x / result y)
 eval (Multiplication x y) = (foursCount [x, y], result x * result y)
 eval (Pow x y)            = (foursCount [x, y], result x ** result y)
 eval (Root x y)           = (foursCount [x, y], result x ** (1 / (result y)))
-eval (Concat x y)         = (foursCount [x, y], read $ (show (floor (result x))) ++ (show (abs (floor (result y)))))
-eval (Square x)           = (foursCount [x]   , (result x) ** 2)
+eval (Factorial x)        = (foursCount [x]   , product [1..(result x)])
 eval (SquareRoot x)       = (foursCount [x]   , sqrt (result x))
+eval (Termial x)          = (foursCount [x]   , sum [1..(result x)])
 
 instance Show (Operation Value) where
-  show NoOp                 = "4"
+  show Four                 = "4"
   show (Sum x y)            = "(" ++ (show x) ++ " + " ++ (show y) ++ ")"
   show (Minus x y)          = "(" ++ (show x) ++ " - " ++ (show y) ++ ")"
   show (Division x y)       = "(" ++ (show x) ++ " / " ++ (show y) ++ ")"
   show (Multiplication x y) = "(" ++ (show x) ++ " * " ++ (show y) ++ ")"
   show (Pow x y)            = "(" ++ (show x) ++ " ^ " ++ (show y) ++ ")"
   show (Root x y)           = "(" ++ (show x) ++ "√ " ++ (show y) ++ ")"
-  show (Concat x y)         = "(" ++ (show x) ++ " ++ " ++ (show y) ++ ")"
-  show (Square x)           = (show x) ++ "²"
+  show (Factorial x)        = (show x) ++ "!"
   show (SquareRoot x)       = "√" ++ (show x)
+  show (Termial x)          = (show x) ++ "?"
 
 result :: Operation Value -> Value
 result = snd . eval
 
 foursCount :: [Operation Value] -> Int
-foursCount ops = sum $ fmap (fst . eval) ops
+foursCount ops = sum ((fst . eval) <$> ops)
 
 possibleCombinations1 :: [Operation Value]
-possibleCombinations1 = [NoOp] ++ [ x NoOp | x <- singleops ]
+possibleCombinations1 = [Four] ++ [ x y | x <- singleops, y <- noops ]
 
-possibleCombinations2 :: [Operation Value] -> [Operation Value]
-possibleCombinations2 []     = [ op x y | op <- doubleops, x <- possibleCombinations1, y <- possibleCombinations1 ]
-possibleCombinations2 (x:xs) = [ op x y | op <- doubleops, x <- (possibleCombinations1 ++ possibleCombinations2 xs), y <- (possibleCombinations1 ++ possibleCombinations2 xs) ]
+possibleCombinations2 :: Int -> [Operation Value]
+possibleCombinations2 0      = [ op x y | op <- doubleops, x <- possibleCombinations1, y <- possibleCombinations1 ]
+possibleCombinations2 opsnum = [ op x y | op <- doubleops, x <- (possibleCombinations1 ++ possibleCombinations2 (opsnum - 1)), y <- (possibleCombinations1 ++ possibleCombinations2 (opsnum - 1)) ]
 
-findOperationWithFourResultingIn x = listToMaybe [ op | op <- possibleCombinations2 [NoOp, NoOp], result op == x, foursCount [op] == 4 ]
+findOperationWithFourResultingIn x = listToMaybe [ op | op <- possibleCombinations2 1, result op == x, foursCount [op] == 4 ]
+
