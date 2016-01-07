47a52e21417d2264ba751506540e6487eb24f4ad
Rogério Chaves <rogeriocfj@gmail.com>
2016-01-07 15:42:30 -0200

    Linting

--- Main.hs
+++ Main.hs
@@ -26,10 +26,10 @@ eval (Minus x y)          = eval x - eval y
 eval (Division x y)       = eval x / eval y
 eval (Multiplication x y) = eval x * eval y
 eval (Pow x y)            = eval x ** eval y
-eval (Root x y)           = eval x ** (1 / (eval y))
-eval (Factorial x)        = product [1..(eval x)]
+eval (Root x y)           = eval x ** (1 / eval y)
+eval (Factorial x)        = product [1..eval x]
 eval (SquareRoot x)       = sqrt (eval x)
-eval (Termial x)          = sum [1..(eval x)]
+eval (Termial x)          = sum [1..eval x]
 
 foursCount :: Operation Value -> Int
 foursCount Four                 = 1
@@ -45,25 +45,31 @@ foursCount (Termial x)          = foursCount x
 
 instance Show (Operation a) where
   show Four                 = "4"
-  show (Sum x y)            = "(" ++ (show x) ++ " + " ++ (show y) ++ ")"
-  show (Minus x y)          = "(" ++ (show x) ++ " - " ++ (show y) ++ ")"
-  show (Division x y)       = "(" ++ (show x) ++ " / " ++ (show y) ++ ")"
-  show (Multiplication x y) = "(" ++ (show x) ++ " * " ++ (show y) ++ ")"
-  show (Pow x y)            = "(" ++ (show x) ++ " ^ " ++ (show y) ++ ")"
-  show (Root x y)           = "(" ++ (show x) ++ "√ " ++ (show y) ++ ")"
-  show (Factorial x)        = (show x) ++ "!"
-  show (SquareRoot x)       = "√" ++ (show x)
-  show (Termial x)          = (show x) ++ "?"
+  show (Sum x y)            = "(" ++ show x ++ " + " ++ show y ++ ")"
+  show (Minus x y)          = "(" ++ show x ++ " - " ++ show y ++ ")"
+  show (Division x y)       = "(" ++ show x ++ " / " ++ show y ++ ")"
+  show (Multiplication x y) = "(" ++ show x ++ " * " ++ show y ++ ")"
+  show (Pow x y)            = "(" ++ show x ++ " ^ " ++ show y ++ ")"
+  show (Root x y)           = "(" ++ show x ++ "√ " ++ show y ++ ")"
+  show (Factorial x)        = show x ++ "!"
+  show (SquareRoot x)       = "√" ++ show x
+  show (Termial x)          = show x ++ "?"
 
 possibleCombinations1 :: [Operation Value]
 possibleCombinations1 = noops ++ [ x y | x <- singleops, y <- noops ]
 
 possibleCombinations2 :: Int -> [Operation Value]
-possibleCombinations2 0      = [ op x y | op <- doubleops, x <- possibleCombinations1, y <- possibleCombinations1 ]
-possibleCombinations2 opsnum = [ op x y | op <- doubleops, x <- (possibleCombinations1 ++ possibleCombinations2 (opsnum - 1)), y <- (possibleCombinations1 ++ possibleCombinations2 (opsnum - 1)) ]
+possibleCombinations2 0      = [ op x y | op <- doubleops,
+                                          x <- possibleCombinations1, y <- possibleCombinations1 ]
+possibleCombinations2 opsnum = [ op x y | op <- doubleops,
+                                          x <- possibleCombinations1 ++ possibleCombinations2 (opsnum - 1),
+                                          y <- possibleCombinations1 ++ possibleCombinations2 (opsnum - 1) ]
 
-findOperationWithFourResultingIn x = listToMaybe [ op | op <- possibleCombinations2 1, eval op == x, foursCount op == 4 ]
 
 main = do
   number <- getArgs
   print $ findOperationWithFourResultingIn $ read $ head number
+
+findOperationWithFourResultingIn x = listToMaybe [ op | op <- possibleCombinations2 1,
+                                                        eval op == x,
+                                                        foursCount op == 4 ]
